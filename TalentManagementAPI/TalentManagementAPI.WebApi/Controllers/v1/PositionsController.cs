345a7a561dc0e2d03961911a5deb6368ee1284d5
Fuji <fuji.nguyen@workcontrol.com>
2023-04-23 20:10:54 -0400

    Code clean up

--- TalentManagementAPI/TalentManagementAPI.WebApi/Controllers/v1/PositionsController.cs
+++ TalentManagementAPI/TalentManagementAPI.WebApi/Controllers/v1/PositionsController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Claims;
 using System.Threading.Tasks;
